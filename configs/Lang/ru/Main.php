55b9f8c45b08c570e6ae91e839cbfded9fda4edd
Lucas Moreira <hehetri@gmail.com>
2025-12-13 20:53:17 +0000

    Delete configs directory

--- configs/Lang/ru/Main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$Lang['LoginTitle'] = "Авторизация";
-$Lang['LoginLogin'] = "Логин";
-$Lang['LoginPassword'] = "Пароль";
-$Lang['LoginCaptcha'] = "Капча";
-$Lang['LoginButton'] = "Войти";
-$Lang['LoginRegister'] = "Регистрация";
-$Lang['LoginRestore'] = "Востановление пароля";
--- configs/Lang/ru/StatisticController/actionGuilds.php
+++
@@ -1,9 +0,0 @@
-<?php
-$Lang['TopGuildsTitle'] = "Рейтинг гилдий";
-$Lang['TopGuildsListEmpty'] = "Ни найдено ни одной гилдии";
-$Lang['TopGuildsNomer'] = "№";
-$Lang['TopGuildsName'] = "Название";
-$Lang['TopGuildsScore'] = "Очков";
-$Lang['TopGuildsMaster'] = "Мастер";
-$Lang['TopGuildsMark'] = "Эмблема";
-$Lang['TopGuildsMembers'] = "Участников";
