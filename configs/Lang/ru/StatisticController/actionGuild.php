7abb694086337e49b0bebbd94764c867acaaacec
Lucas Moreira <hehetri@gmail.com>
2025-12-13 21:52:19 +0000

    Delete configs directory

--- configs/Lang/ru/StatisticController/actionGuild.php
+++
@@ -1,7 +0,0 @@
-<?php
-$Lang['ViewGuildTitle'] = "Guild: ";
-$Lang['ViewGuildName'] = "Guild Name";
-$Lang['ViewGuildMaster'] = "Guild Master";
-$Lang['ViewGuildScore'] = "Score";
-$Lang['ViewGuildMembName'] = "Member Name";
-$Lang['ViewGuildMembStatus'] = "Member Status";
